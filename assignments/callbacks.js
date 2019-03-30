894a06d1c7239ff35da04c510b382c080cf14cc4
Dominic Torruellas <dom@domtorr.com>
2019-03-30 08:52:56 -0700

    init commit and formatting

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,11 +1,14 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your
+// work. You have been provided an example of a problem and a solution to see
+// how this works with our items array.  Study both the problem and the solution
+// to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -17,13 +20,12 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     return cb(arr[0]);
   }
 
-  // Function invocation 
+  // Function invocation
   firstItem(items, function(first) {
     console.log(first)
   });
 
 */
-
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,22 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
-
-/* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
+/* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks
+for today's project files */
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  // Return a function that when invoked increments and returns a counter
+  // variable.
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
-// ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// ==== Challenge 3: Create a counter function with an object that can increment
+// and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  // `increment` should increment a counter variable in closure scope and return
+  // it. `decrement` should decrement the counter variable and return it.
 };
